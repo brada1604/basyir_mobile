6b14a9794f5ab6fca10e5b4004f164fa55e2d3e1
Rivan20 <95005451+Rivan20@users.noreply.github.com>
2023-05-26 02:21:03 +0700

    Update Amalan Yaumi

--- app/Views/amalan_yaumi/v_amalan_yaumi.php
+++ app/Views/amalan_yaumi/v_amalan_yaumi.php
@@ -6,25 +6,22 @@
             </a>
         </nav>
         <div class="container">
-            <div class="featured-carousel owl-carousel">
-                <?php foreach ($getAmalanYaumiByStatus as $row) : ?>
-                    <div class="row">
-                        <div class="col-md-2 text-center">
-                            <h2 class="heading-section mb-5  bold-text" style="font-family: 'Inter',Arial, sans-serif; color: white; font-weight: bold;"><a href="/rencana_kegiatan/add/<?= $row->id_amalan_yaumi ?>"><?= $row->judul_amalan_yaumi ?></a></h2>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="d-flex align-items-end justify-content-center" style="background-color: white; border-radius: 20px; height: 230px;">
+            <div class="row">
+                <div class="col-md-2 text-center">
+                    <h2 class="heading-section mb-5  bold-text" style="font-family: 'Inter',Arial, sans-serif; color: white; font-weight: bold;">Amalan Yaumi</h2>
+                </div>
+                <div class="col-md-6">
+                    <div class="featured-carousel owl-carousel">
+                        <?php foreach ($getAmalanYaumiByStatus as $row) : ?>
+                            <div class="d-flex align-items-end justify-content-center" style="background-color: white; border-radius: 20px;">
                                 <div class="text w-100">
-                                    <nav class=" navbar container">
-                                        <h6 style="font-family: 'Inter',Arial, sans-serif; color: #629C87; margin-bottom: 35px;"><?= $row->konten_amalan_yaumi ?></h6>
-                                    </nav>
+                                    <h4 class="container" style="font-family: 'Inter',Arial, sans-serif; color: #629C87; font-weight: bold;"><a href="/rencana_kegiatan/add/<?= $row->id_amalan_yaumi ?>"><?= $row->judul_amalan_yaumi ?></a></h4>
+                                    <h5 class="container" style="font-family: 'Inter',Arial, sans-serif; color: #629C87;"><?= $row->konten_amalan_yaumi ?></h5>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                <?php endforeach; ?>
-                <!--  -->
-
+                        <?php endforeach; ?>
+                    </div>  
+                </div>
             </div>
         </div>
     </div>
