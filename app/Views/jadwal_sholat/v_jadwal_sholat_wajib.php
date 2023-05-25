f768112389ebd6c5ce082b5d58820a449c42e377
Rivan20 <95005451+Rivan20@users.noreply.github.com>
2023-05-26 03:11:56 +0700

    Update Shalat

--- app/Views/jadwal_sholat/v_jadwal_sholat_wajib.php
+++ app/Views/jadwal_sholat/v_jadwal_sholat_wajib.php
@@ -11,13 +11,19 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-2 text-center">
-                    <h2 class="heading-section mb-5  bold-text" style="font-family: 'Inter',Arial, sans-serif; color: white; font-weight: bold;">Solat Wajib</h2>
+                    <h2 class="heading-section mb-5  bold-text" style="font-family: 'Inter',Arial, sans-serif; color: white; font-weight: bold;">Bandung</h2>
+                    <h2 style="font-family: 'Inter',Arial, sans-serif; color: white; font-weight: bold;">
+                        <p id="DisplayClock" class="clock" onload="showTime()" style="margin-top: -17%"></p>
+                    </h2>
+                    <h5 style="font-family: 'Inter',Arial, sans-serif; color: white; font-weight: bold;">
+                        <p id="DisplayDate" onload="showDate()" style="margin-top: -7%"></p>
+                    </h5>
                 </div>
                 <div class="col-md-6">
                     <div class="d-flex align-items-end justify-content-center" style="background-color: white; border-radius: 20px; height: 80px; margin-top: 30px">
                         <div class="text w-100">
                             <nav class=" navbar container">
-                                <h4 style="font-family: 'Inter',Arial, sans-serif; color: #629C87; margin-bottom:15px;">Imsak</h4>
+                                <h4 style="font-family: 'Inter',Arial, sans-serif; color: #629C87; margin-bottom:15px; font-weight: bold;">Imsak</h4>
                                 <h4 style="font-family: 'Inter',Arial, sans-serif; color: #629C87; margin-bottom:15px;"><?= $pengingat_imsak; ?></h4>
                             </nav>
                         </div>
@@ -28,7 +34,8 @@
                     <div class="d-flex align-items-end justify-content-center" style="background-color: white; border-radius: 20px; height: 80px; margin-top: 20px">
                         <div class="text w-100">
                             <nav class=" navbar container">
-                                <h4 style="font-family: 'Inter',Arial, sans-serif; color: #629C87; margin-bottom:15px;">Subuh (Fajr)</h4>
+                                <h4 style="font-family: 'Inter',Arial, sans-serif; color: #629C87; margin-bottom:15px; font-weight: bold;">Subuh (Fajr)</h4>
+                                <img src="/assets/assets-mobile/icon-menu/sunrise.png" alt="sunrise" width="30em" height="30em" style="margin-top:-7%; margin-left:20%;" />
                                 <h4 style="font-family: 'Inter',Arial, sans-serif; color: #629C87; margin-bottom:15px;"><?= $pengingat_subuh; ?></h4>
                             </nav>
                         </div>
@@ -39,7 +46,8 @@
                     <div class="d-flex align-items-end justify-content-center" style="background-color: white; border-radius: 20px; height: 80px; margin-top: 20px">
                         <div class="text w-100">
                             <nav class=" navbar container">
-                                <h4 style="font-family: 'Inter',Arial, sans-serif; color: #629C87; margin-bottom:15px;">Dzuhur</h4>
+                                <h4 style="font-family: 'Inter',Arial, sans-serif; color: #629C87; margin-bottom:15px; font-weight: bold;">Dzuhur</h4>
+                                <img src="/assets/assets-mobile/icon-menu/sun1.png" alt="sunrise" width="30em" height="30em" style="margin-top:-7%; margin-left:38%;" />
                                 <h4 style="font-family: 'Inter',Arial, sans-serif; color: #629C87; margin-bottom:15px;"><?= $pengingat_dzuhur; ?></h4>
                             </nav>
                         </div>
@@ -50,7 +58,8 @@
                     <div class="d-flex align-items-end justify-content-center" style="background-color: white; border-radius: 20px; height: 80px; margin-top: 20px">
                         <div class="text w-100">
                             <nav class=" navbar container">
-                                <h4 style="font-family: 'Inter',Arial, sans-serif; color: #629C87; margin-bottom:15px;">Ashar</h4>
+                                <h4 style="font-family: 'Inter',Arial, sans-serif; color: #629C87; margin-bottom:15px; font-weight: bold;">Ashar</h4>
+                                <img src="/assets/assets-mobile/icon-menu/sun2.png" alt="sunrise" width="37em" height=37em" style="margin-top:-7%; margin-left:40%;" />
                                 <h4 style="font-family: 'Inter',Arial, sans-serif; color: #629C87; margin-bottom:15px;"><?= $pengingat_ashar; ?></h4>
                             </nav>
                         </div>
@@ -61,7 +70,8 @@
                     <div class="d-flex align-items-end justify-content-center" style="background-color: white; border-radius: 20px; height: 80px; margin-top: 20px">
                         <div class="text w-100">
                             <nav class=" navbar container">
-                                <h4 style="font-family: 'Inter',Arial, sans-serif; color: #629C87; margin-bottom:15px;">Maghrib</h4>
+                                <h4 style="font-family: 'Inter',Arial, sans-serif; color: #629C87; margin-bottom:15px; font-weight: bold;">Maghrib</h4>
+                                <img src="/assets/assets-mobile/icon-menu/sunset.png" alt="sunrise" width="40em" height="40em" style="margin-top:-7%; margin-left:31%;" />
                                 <h4 style="font-family: 'Inter',Arial, sans-serif; color: #629C87; margin-bottom:15px;"><?= $pengingat_maghrib; ?></h4>
                             </nav>
                         </div>
@@ -72,16 +82,20 @@
                     <div class="d-flex align-items-end justify-content-center" style="background-color: white; border-radius: 20px; height: 80px; margin-top: 20px;">
                         <div class="text w-100">
                             <nav class=" navbar container">
-                                <h4 style="font-family: 'Inter',Arial, sans-serif; color: #629C87; margin-bottom:15px;">Isya</h4>
+                                <h4 style="font-family: 'Inter',Arial, sans-serif; color: #629C87; margin-bottom:15px; font-weight: bold;">Isya</h4>
+                                <img src="/assets/assets-mobile/icon-menu/night.png" alt="sunrise" width="35em" height="35em" style="margin-top:-7%; margin-left:50%;" />
                                 <h4 style="font-family: 'Inter',Arial, sans-serif; color: #629C87; margin-bottom:15px;"><?= $pengingat_isya; ?></h4>
                             </nav>
                         </div>
                     </div>
                 </div>
                 <!--  -->
-                <span style="margin-top: 10%;" />
+                <div style="margin-top: 10%;">
+                    <h1>
+                        <p>.</p>
+                    </h1>
+                </div>
             </div>
         </div>
     </div>
-    <br>
 </body>
